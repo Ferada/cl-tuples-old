8e7033405b838c82241d3f436537a332391b96bf
John Connors <johnc@yagc.ndo.co.uk>
2008-05-01 00:00:12 +0100

    Systemizing tests

--- tuples-test.lisp
+++ tuples-test.lisp
@@ -6,9 +6,6 @@
 (in-package :cl-tuples-test)
 
 
-(def-tuple-type pair
-    :tuple-element-type (unsigned-byte 8)
-    :elements (a b))
 
 ;; (cl-tuples::def-tuple pair)
 ;; (cl-tuples::def-tuple-array-dimensions pair)
@@ -30,37 +27,79 @@
 ;; (cl-tuples::def-tuple-map pair)
 ;; (cl-tuples::def-tuple-reduce pair)
 
-(defparameter *test-pair* (make-pair (pair-tuple 1 2)))
-(defparameter *pair-array* (make-pair-array 2 :adjustable t :fill-pointer 1))
 
-(setf *test-pair* (make-pair #{ 3 4 }))
 
-(pair-aref *pair-array* 0)
+(defmacro with-test (test-sym test &rest forms)
+  (cl-tuples::with-gensyms (result)
+    `(progn
+       ,@forms
+       (let 
+           ((,result ,test))
+         (assert ,result)
+         (setf ,test-sym (and ,test-sym ,result)))))
 
-(setf (aref *pair-array* 0) (pair  *test-pair*))
 
-(pair-vector-push (pair *test-pair*) *pair-array*)
+(defparameter *result* t)
 
-(pair-vector-push-extend (pair-tuple 6 7) *pair-array*)
+(def-tuple-type pair
+    :tuple-element-type (unsigned-byte 8)
+    :elements (a b))
 
-(map-pair-tuples #'+ #{ 1 2 } #{ 2 3 } #{ 4 5 })
+;; basic operations
+(with-test *result*
+  (and (equalp *test-pair* #(3 4))
+       (equalp *pair-array* #( 3 4)))
+  (defparameter *test-pair* (make-pair (pair-tuple 1 2)))
+  (defparameter *pair-array* (make-pair-array 2 :adjustable t :fill-pointer 1))
+  (setf *test-pair* (make-pair #{ 3 4 }))
+  (setf (pair-aref *pair-array* 0) (pair *test-pair*)))
 
-(reduce-pair-tuple #'* #{ 2 2 })
 
+;; array extension
+(with-test *result*
+  (equalp *pair-array* #(3 4 3 4 6 7))
+  (pair-vector-push (pair *test-pair*) *pair-array*)
+  (pair-vector-push-extend (pair-tuple  6 7) *pair-array*))
+
+;; map/reduce
+(with-test *result* 
+  (and (equalp *test-pair* #(7 10))
+       (= *result* 4))
+  (setf *test-pair* (make-pair (map-pair-tuples #'+ #{ 1 2 } #{ 2 3 } #{ 4 5 })))
+  (setf *result* (reduce-pair-tuple #'* #{ 2 2 })))
+
+;; basic vector math
 (defparameter *vector0* (make-vector3d #{ 0.0 0.0 0.0 } )) 
 (defparameter *vector1* (make-vector3d #{ 1.0 1.0 1.0 } ))
 (defparameter *vectorx* (make-vector3d #{ 1.0 0.0 0.0 } ))
 (defparameter *vectory* (make-vector3d #{ 0.0 1.0 0.0 } ))
 (defparameter *vectorz* (make-vector3d #{ 0.0 0.0 1.0 } ))
 
+(defparameter *test-vector* (new-vector3d))
+
 (map-vector3d-tuples #'+ (vector3d  *vector0*) (vector3d *vector1*))
 (reduce-vector3d-tuple #'- (vector3d *vector1*))
 
-(vector3d-length (vector3d *vector0*))
-(vector3d-length (vector3d *vector1*))
-(vector3d-normal (vector3d *vector1*))
-(vector3d-cross (vector3d *vectorx*) (vector3d *vectory*))
-(vector3d-dot (vector3d *vectorx*) (vector3d-normal  (vector3d *vector1*)))
+(with-test *result*
+  (= *result* 0.0)
+  (setf *result* (vector3d-length (vector3d *vector0*))))
+
+(with-test *result*
+  (= *result* (sqrt 3.0))
+  (setf *result* (vector3d-length (vector3d *vector1*))))
+
+(with-test *result*
+ (equalp *test-vector* #( 0.57735026 0.57735026 0.57735026))
+ (setf *test-vector* (make-vector3d (vector3d-normal (vector3d *vector1*)))))
+
+(with-test *result*
+  (equalp *test-vector* #(0.0 0.0 1.0))
+  (setf *text-vector* (vector3d-cross (vector3d *vectorx*) (vector3d *vectory*))))
+
+(with-test *result*
+  (= *result* 0.57735026)
+  (setf *result* (vector3d-dot 
+                  (vector3d *vectorx*) (vector3d-normal  (vector3d *vector1*)))))
 
 (defparameter *vertex0* (make-vertex3d (vector3d-vertex3d (vector3d  *vector0*))))
 (defparameter *vertex1* (make-vertex3d (vector3d-vertex3d (vector3d  *vector1*))))
@@ -68,9 +107,15 @@
 (defparameter *vertexy* (make-vertex3d (vector3d-vertex3d (vector3d  *vectory*))))
 (defparameter *vertexz* (make-vertex3d (vector3d-vertex3d (vector3d  *vectorz*))))
 
-(delta-vector3d (vector3d  *vertex0*) (vector3d *vertex1*))
-(vertex3d-distance (vector3d  *vertex0*) (vector3d  *vertex1*))
+(with-test *result*
+  (equalp *test-vector* #(1.0 1.0 1.0))
+  (setf *test-vector* (make-vector3d (delta-vector3d (vector3d  *vertex0*) (vector3d *vertex1*)))))
 
+(with-test *result*
+  (= *result* 1.7320508)
+  (vertex3d-distance (vector3d  *vertex0*) (vector3d  *vertex1*)))
+
+;; basic matrix math
 (defparameter *rotatex* (make-matrix44 (rotatex-matrix44 45)))
 (defparameter *rotatey* (make-matrix44 (rotatey-matrix44 45)))
 (defparameter *rotatez* (make-matrix44 (rotatez-matrix44 45)))

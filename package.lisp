64a02cea569692b43d47ae1741a83087c7aeab16
John Connors <johnc@yagc.ndo.co.uk>
2011-10-05 21:14:40 +0100

    Actually export correct symbols from the package.

--- package.lisp
+++ package.lisp
@@ -17,62 +17,60 @@
            def-tuple-type
            def-tuple-op
 
-           vector2d-dot
-           vector2d-mag-square
-           vector2d-length
-           vector2d-normal
-           vector2d-vertex2d
+           vector2d-dot*
+           vector2d-mag-square*
+           vector2d-length*
+           vector2d-normal*
+           vector2d-vertex2d*
 
-           vertex2d-vector2d
+           vertex2d-vector2d*
 
-           vector3d-scale
-           vector3d-dot
-           vector3d-sum
-           vector3d-difference
-           vector3d-mag-square
-           vector3d-length
-           vector3d-normal
-           vector3d-cross
-           vector3d-vertex3d
-           vertex3d-vector3d
-           vertex3d-distance
+           vector3d-scale*
+           vector3d-dot*
+           vector3d-sum*
+           vector3d-difference*
+           vector3d-mag-square*
+           vector3d-length*
+           vector3d-normal*
+           vector3d-cross*
+           vector3d-vertex3d*
+           vertex3d-vector3d*
+           vertex3d-distance*
 
-           delta-vector3d 
+           delta-vector3d*
 
-           transpose-matrix33
-           identity-matrix44
-           translation-matrix44
-           vertex3d-translation-matrix44
-           rotatex-matrix44
-           rotatey-matrix44
-           rotatez-matrix44
-           transpose-matrix44
+           transpose-matrix33*
+           identity-matrix44*
+           translation-matrix44*
+           vertex3d-translation-matrix44*
+           rotatex-matrix44*
+           rotatey-matrix44*
+           rotatez-matrix44*
+           transpose-matrix44*
 
-           transform-vertex2d
-           transform-vertex3d
-           transform-vector2d
-           transform-vector3d
-           matrix33-product
-           matrix44-product
-           matrix44-matrix33
-           matrix33-matrix44
+           transform-vertex2d*
+           transform-vertex3d*
+           transform-vector2d*
+           transform-vector3d*
+           matrix33-product*
+           matrix44-product*
+           matrix44-matrix33*
+           matrix33-matrix44*
 
-           quaternion-sum
-           quaternion-unitize
-           quaternion-norm
-           quaternion-scale
-           quaternion-conjugate
-           quaternion-dot
-           quaternion-mag-square
-           quaternion-mag
-           quaternion-inverse
-           quaternion-product
-           quaternion-matrix33
-           angle-axis-quaternion
-           quaternion-transform-vector3d
-           vector3d-quaternion
-
-           def-tuple-class
+           quaternion-sum*
+           quaternion-unitize*
+           quaternion-norm*
+           quaternion-scale*
+           quaternion-conjugate*
+           quaternion-dot*
+           quaternion-mag-square*
+           quaternion-mag*
+           quaternion-inverse*
+           quaternion-product*
+           quaternion-matrix33*
+           angle-axis-quaternion*
+           quaternion-transform-vector3d*
+           vector3d-quaternion*
 
            width
            height)
